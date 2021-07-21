b47e382b063140c51c9be021bb1a1bf0d48d7275
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-07-21 13:49:15 +0300

    Constructor JS day1

--- js/main.js
+++ js/main.js
@@ -26,28 +26,60 @@
 
 	 */
 
-	const getElemment = (tagName, classNames) => {
+	const getElement = (tagName, classNames, attributes) => {
 		const element = document.createElement(tagName);
 		if (classNames) {
 			element.classList.add(...classNames);
+		}
+
+		if (attributes) {
+			for (const attribute in attributes) {
+				element[attribute] = attributes[attribute];
+			}
 		}
 		return element;
 	}
 
 	const createHeader = (param) => {
-		const header = getElemment('header');
+		const header = getElement('header');
 
-		const container = getElemment('div', ['container']);
+		const container = getElement('div', ['container']);
 
-		const wrapper = getElemment('div', ['header']);
+		const wrapper = getElement('div', ['header']);
 
 		if (param.header.logo) {
-			const logo = getElemment('img', ['logo']);
-			logo.src = param.header.logo;
-			logo.alt = 'Логотип ' + param.title;
+			const logo = getElement('a', ['logo-link'],);
+
+			const logoImage = getElement('img', ['logo-image'],{ 
+				src: param.header.logo,
+				alt: 'Логотип ' + param.title,
+			});
+			logo.append(logoImage)
 			wrapper.append(logo);
 		}
 
+		if (param.header.social) {
+			const socialWrapper = getElement('div', ['social']);
+			const allSocial = param.header.social.map (item => {
+				const socialLink = getElement('a', ['social-link']);
+				socialLink.append(getElement('img', [], {
+					src: item.image,
+					alt: item.title
+				}));
+
+				socialLink.href = item.link;
+
+				return socialLink;
+
+			})
+
+			socialWrapper.append(...allSocial);
+			wrapper.append(socialWrapper);
+		}
+
+		if (param.header.menu) {
+			// домашнее задание
+		}
 
 		header.append(container);
 		container.append(wrapper);
@@ -57,7 +89,7 @@
 
 	}
 
-	const moveConstructor = (selector, options) => {
+	const movieConstructor = (selector, options) => {
 			const app = document.querySelector(selector);
 
 			app.classList.add('body-app')
@@ -70,10 +102,41 @@
 
 	};
 
-	moveConstructor('.app', {
+	movieConstructor('.app', {
 		title: 'Ведьмак',
 		header:{
-			logo: 'witcher/logo.png'
+			logo: 'witcher/logo.png',
+			social: [
+				{
+					title: 'Twitter',
+					link: 'https://twitter.com',
+					image: 'witcher/social/twitter.svg',
+				},
+				{
+					title: 'Instagram',
+					link: 'https://instagram.com',
+					image: 'witcher/social/instagram.svg',
+				},
+				{
+					title: 'Facebook',
+					link: 'https://facebook.com',
+					image: 'witcher/social/facebook.svg',
+				}
+			],
+			menu: [
+				{
+					title: 'Описание',
+					link: '#',
+				},
+				{
+					title: 'Трейлер',
+					link: '#',
+				},
+				{
+					title: 'Отзывы',
+					link: '#',
+				},
+			]
 		}
 
 	});
